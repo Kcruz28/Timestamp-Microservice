aa8d77360816ad42ad0ae74c6bbfb5f6cc87f92a
Kevin <kcruz28@illinois.edu>
2024-06-04 08:41:37 -0500

    first commit

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/2015-12-25", function (req, res) {
+  res.json({"unix": 1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"});
 });
 
 
